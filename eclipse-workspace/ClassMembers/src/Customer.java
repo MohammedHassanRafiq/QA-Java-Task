08e79e0c13c92a1f9508ed08155cd2f9b9c57030
MohammedHassanRafiq <110388368+MohammedHassanRafiq@users.noreply.github.com>
2022-08-08 16:31:41 +0100

    Add files via upload

---
+++ eclipse-workspace/ClassMembers/src/Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+    private String firstName;
+    private String surname;
+
+    public Customer(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ eclipse-workspace/ClassMembers/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer firstCustomer = new Customer("Arnold","Rimmer");
+        Customer secondCustomer = new Customer("David","Lister");
+
+        firstCustomer.setFirstName("Arnold J.");
+        secondCustomer.setFirstName("Dave");
+
+        System.out.println(firstCustomer.getFirstName());   // output: Arnold J.
+        System.out.println(secondCustomer.getFirstName());  // output: Dave
+    }
+}
